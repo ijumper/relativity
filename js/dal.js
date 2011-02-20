90d12e7c606b88cc90a2e986b71203cf8ff459f0
Brian Mason <brian@ijump.co.uk>
2011-02-20 22:30:24 +0000

    second commit

---
+++ js/dal.js
@@ -0,0 +1,21 @@
+var dataAccessLayer = function (database) {
+
+	
+	var db = database;
+	
+	executeSql : function (sql) {
+
+
+	}
+
+	
+	checkDatabaseExists : function () {
+
+
+	}
+	
+	createDatabase : function (sql) {
+
+
+	}	
+}
