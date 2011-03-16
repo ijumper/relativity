2c398ecabf6eeca664c9a5eba4b2e7e4d7bedbbf
bmason <brian.mason@nokia.com>
2011-03-16 23:00:01 +0000

    added test libs

---
+++ src/dal.js
@@ -0,0 +1,25 @@
+var dal = {
+
+	var db = databaseName;
+	
+	initializeDatabase : function (databaseName) {
+	
+		db = databaseName;
+	
+	}
+	
+	executeSql : function (sql) {
+
+	}
+
+	
+	checkDatabaseExists : function () {
+
+
+	}
+	
+	createDatabase : function (sql) {
+
+
+	}	
+}
